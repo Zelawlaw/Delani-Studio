d3eeeb113986f1b1c35f52e435d109d7c12b10bc
Lawrence Karani <lawrencekarani@gmail.com>
2022-11-06 02:41:38 +0300

    added js files & icon click functionality

---
+++ js/uiscript.js
@@ -0,0 +1,33 @@
+$(document).ready(()=>{
+ 
+  const varyheight = ()=>{
+    if ( $('#desdesc').css('display') == 'none' && $('#devdesc').css('display') == 'none' && $('#projdesc').css('display') == 'none' )
+     {
+      $("section#what_we_do.page-section").css("padding-bottom",'20rem');
+     }
+     else {
+      $("section#what_we_do.page-section").css("padding-bottom",'40rem');
+     }
+  };
+
+$("#design").click(()=>{
+ 
+  $("#desimg").toggle();
+  $("#desdesc").toggle();
+  varyheight();
+});
+$("#development").click(()=>{
+ 
+  $("#devimg").toggle();
+  $("#devdesc").toggle();
+  varyheight();
+});
+$("#projmanagement").click(()=>{
+ 
+  $("#projimg").toggle();
+  $("#projdesc").toggle();
+  varyheight();
+});
+  
+}
+);
