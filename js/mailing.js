c5951ceea767e6439a33ef2daba738b913fc4c71
Lawrence Karani <lawrencekarani@gmail.com>
2022-11-06 19:27:23 +0300

    reformatting

--- js/mailing.js
+++ js/mailing.js
@@ -1,27 +1,19 @@
-$(document).ready(()=>{
-  
+$(document).ready(() => {
   const pattern = /^\b[A-Z0-9._%-]+@[A-Z0-9.-]+\.[A-Z]{2,4}\b$/i;
 
- $("#submitform").click((e)=>{
+  $("#submitform").click((e) => {
+    let name = $("#name").val();
+    let email = $("#email").val();
+    let message = $("#message").val();
 
-  let name = $("#name").val();
-  let email= $("#email").val();
-  let message = $("#message").val();
-  
-  
- 
-  if(!pattern.test(email)){
-    alert("Email format is incorrect! Kindly use format abc@def.ghi");
-  }
-  else{
-   alert("Feeback received");
-   $("#name").val("");
-   $("#email").val("");
-   $("#message").val("");
-  }
-  e.preventDefault();
- });
-
-  
-}
-);
+    if (!pattern.test(email)) {
+      alert("Email format is incorrect! Kindly use format abc@def.ghi");
+    } else {
+      alert("Feeback received");
+      $("#name").val("");
+      $("#email").val("");
+      $("#message").val("");
+    }
+    e.preventDefault();
+  });
+});
--- js/uiscript.js
+++ js/uiscript.js
@@ -1,34 +1,23 @@
-$(document).ready(()=>{
+$(document).ready(() => {
+  $("#design").click(() => {
+    $("#desimg").toggle();
+    $("#desdesc").toggle();
+  });
+  $("#development").click(() => {
+    $("#devimg").toggle();
+    $("#devdesc").toggle();
+  });
+  $("#projmanagement").click(() => {
+    $("#projimg").toggle();
+    $("#projdesc").toggle();
+  });
 
-
-$("#design").click(()=>{
- 
-  $("#desimg").toggle();
-  $("#desdesc").toggle();
-
+  $(".gallery-item").hover(
+    function () {
+      $(this).find(".img-title").fadeIn(300);
+    },
+    function () {
+      $(this).find(".img-title").fadeOut(300);
+    }
+  );
 });
-$("#development").click(()=>{
- 
-  $("#devimg").toggle();
-  $("#devdesc").toggle();
-
-});
-$("#projmanagement").click(()=>{
- 
-  $("#projimg").toggle();
-  $("#projdesc").toggle();
-
-});
-  
-
-$('.gallery-item').hover( function() {
-  
-
-  $(this).find('.img-title').fadeIn(300);
-}, function() {
-  $(this).find('.img-title').fadeOut(300);
-
-});
-
-}
-);
