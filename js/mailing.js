137056b2f771fb0adae66e062d832ddb99b4f3a5
Lawrence Karani <lawrencekarani@gmail.com>
2022-11-06 18:03:44 +0300

    removed default action of scrolling on button click

--- js/mailing.js
+++ js/mailing.js
@@ -1,6 +1,24 @@
 $(document).ready(()=>{
   
+  const pattern = /^\b[A-Z0-9._%-]+@[A-Z0-9.-]+\.[A-Z]{2,4}\b$/i;
 
+ $("#submitform").click((e)=>{
+
+  let name = $("#name").val();
+  let email= $("#email").val();
+  let message = $("#message").val();
+  
+  
+ 
+  if(!pattern.test(email)){
+    alert("Email format is incorrect! Kindly use format abc@def.ghi");
+  }
+  else{
+   alert("send email");
+
+  }
+  e.preventDefault();
+ });
 
   
 }
