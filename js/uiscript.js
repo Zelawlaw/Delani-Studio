60cfb995d75f37f97750c5300a43507b4e822189
Lawrence Karani <lawrencekarani@gmail.com>
2022-11-06 17:40:08 +0300

    remove varyheight method

--- js/uiscript.js
+++ js/uiscript.js
@@ -1,32 +1,23 @@
 $(document).ready(()=>{
- 
-  const varyheight = ()=>{
-    if ( $('#desdesc').css('display') == 'none' && $('#devdesc').css('display') == 'none' && $('#projdesc').css('display') == 'none' )
-     {
-      $("section#what_we_do.page-section").css("padding-bottom",'10rem');
-     }
-     else {
-      $("section#what_we_do.page-section").css("padding-bottom",'20rem');
-     }
-  };
+
 
 $("#design").click(()=>{
  
   $("#desimg").toggle();
   $("#desdesc").toggle();
-//  varyheight();
+
 });
 $("#development").click(()=>{
  
   $("#devimg").toggle();
   $("#devdesc").toggle();
-//  varyheight();
+
 });
 $("#projmanagement").click(()=>{
  
   $("#projimg").toggle();
   $("#projdesc").toggle();
- // varyheight();
+
 });
   
 
